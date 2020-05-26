5247309812428b57c1d8feb66dd80eb4d92b754a
Max R <maxx.ratto@gmail.com>
2020-05-26 08:48:27 -0600

    console log

--- src/users/users-service.js
+++ src/users/users-service.js
@@ -25,7 +25,7 @@ const UsersService = {
         })
         .into('contestphyte_users')
         .returning('*')
-        .then(console.log(hash))
+        .then((rows) => console.log(rows[0]))
         .then((rows) => rows[0]));
   },
   updateUser(knex, id, updatedUser) {
