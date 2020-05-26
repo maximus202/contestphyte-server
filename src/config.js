a75ac71ac2d654aab67066c1af9cdf5cebcea80d
Max R <maxx.ratto@gmail.com>
2020-05-26 08:53:03 -0600

    fixing DB_URL to DATABASE_URL

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DB_URL || 'postgresql://postgres@localhost/contestphytedb',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/contestphytedb',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/contestphytedb-test',
 };
