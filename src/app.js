340424f23f02c4df5db0a7916e48269f772c7e95
Max R <maxx.ratto@gmail.com>
2020-05-26 09:22:54 -0600

    commenting out app.use error

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.use('/api/auth', authRouter);
 app.use('/api/contests', contestsRouter);
 app.use('/api/participants', participantsRouter);
 
-app.use((error, req, res, next) => {
+/*app.use((error, req, res, next) => {
   let response;
   if (NODE_ENV === 'production') {
     response = { error: { message: 'server error' } };
@@ -37,7 +37,7 @@ app.use((error, req, res, next) => {
     response = { message: error.message, error };
   }
   res.status(500).json(response);
-});
+}); */
 
 
 module.exports = app;
